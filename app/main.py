8aa7bb4b97fe02f27bbda423f940a2c8526bcb6e
klyutko7p <klyutko.d@gmail.com>
2024-08-16 01:02:21 +0500

    fsaffas

--- app/main.py
+++ app/main.py
@@ -8,7 +8,6 @@ from selenium.webdriver.support import expected_conditions as EC
 import os
 
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
