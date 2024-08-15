c2b139f7cc05a5a7901b98eb864a41c7590c0711
klyutko7p <klyutko.d@gmail.com>
2024-08-16 01:40:26 +0500

    fsaffas

--- app/main.py
+++ app/main.py
@@ -8,6 +8,7 @@ from selenium.webdriver.support import expected_conditions as EC
 import os
 
 chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--headless=new")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--disable-renderer-backgrounding")
