b3cdb61176b29030e71355d48bcaa6e301f2969f
klyutko7p <klyutko.d@gmail.com>
2024-08-16 01:38:37 +0500

    fsaffas

--- app/main.py
+++ app/main.py
@@ -8,11 +8,21 @@ from selenium.webdriver.support import expected_conditions as EC
 import os
 
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--disable-infobars')
-chrome_options.add_argument('--disable-dev-shm-usage')
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--remote-debugging-port=9222')
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--disable-renderer-backgrounding")
+chrome_options.add_argument("--disable-background-timer-throttling")
+chrome_options.add_argument("--disable-backgrounding-occluded-windows")
+chrome_options.add_argument("--disable-client-side-phishing-detection")
+chrome_options.add_argument("--disable-crash-reporter")
+chrome_options.add_argument("--disable-oopr-debug-crash-dump")
+chrome_options.add_argument("--no-crash-upload")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--disable-extensions")
+chrome_options.add_argument("--disable-low-res-tiling")
+chrome_options.add_argument("--log-level=3")
+chrome_options.add_argument("--silent")
+# chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 
 driver = webdriver.Chrome(options=chrome_options)
 print("Браузер успешно открыт")
@@ -31,7 +41,7 @@ def handle_post():
 
     try:
         price_element = driver.find_elements(By.CLASS_NAME, "m6n_27")[0].text
-        title_element = driver.find_elements(By.CLASS_NAME, "m2o_27")[0].text
+        title_element = driver.find_elements(By.TAG_NAME, "h1")[0].text
         price = price_element
         title = title_element
 
@@ -42,6 +52,3 @@ def handle_post():
 
     response_data = {'status': 'success', 'message': f'{price}, {title}'}
     return jsonify(response_data)
-
-
-driver.quit()
