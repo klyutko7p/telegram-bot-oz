60fc7d5dc984c8e1742ea54f1963b25e4a3260f2
Daniil Klyutko <95645570+klyutko7p@users.noreply.github.com>
2024-09-14 04:08:21 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,40 +5,34 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium_stealth import stealth
+import time
+import json
 import os
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
+options = Options()
+options.add_argument("--headless=new")
 
-driver = webdriver.Chrome(options=chrome_options)
+def init_webdriver():
+    driver = webdriver.Chrome(options=options)
+    stealth(driver, platform="Win32")
+    return driver
+    
 print("Браузер успешно открыт")
-
-from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-
+driver = init_webdriver()
 @app.route('/post_endpoint', methods=['POST'])
 def handle_post():
     url = request.json['url']
     print(url)
-    driver.implicitly_wait(10)
     driver.get(url)
+    time.sleep(5)
+    jsonData = driver.find_element(By.TAG_NAME, "body").text
+    jsonDataReturn = json.loads(jsonData)["seo"]["script"][0]['innerHTML'])
 
-    try:
-        price_element = driver.find_elements(By.CLASS_NAME, "m6n_27")[0].text
-        title_element = driver.find_elements(By.TAG_NAME, "h1")[0].text
-        price = price_element
-        title = title_element
-
-
-    except Exception as e:
-        print("Ошибка при поиске заголовка страницы:", e)
-        return None
-
-    response_data = {'status': 'success', 'message': f'{price}, {title}'}
+    response_data = {'status': 'success', 'message': f'{jsonDataReturn}'}
     return jsonify(response_data)
 
 driver.quit()
